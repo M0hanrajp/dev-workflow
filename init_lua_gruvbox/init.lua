71a8ef07f2728a0d0d39e78d581eed7437320d50
M0hanrajp <mohanrajp.github@gmail.com>
2025-04-11 21:26:51 +0530

    feat(init.lua): Updated configurations with icons for autocompletion

--- init_lua_gruvbox/init.lua
+++ init_lua_gruvbox/init.lua
@@ -750,7 +750,8 @@ require("lazy").setup({
 		},
 	},
 
-	{ -- Autocompletion
+	-- autocompletion-setup
+	{
 		"hrsh7th/nvim-cmp",
 		event = "InsertEnter",
 		dependencies = {
@@ -758,72 +759,38 @@ require("lazy").setup({
 			{
 				"L3MON4D3/LuaSnip",
 				build = (function()
-					-- Build Step is needed for regex support in snippets.
-					-- This step is not supported in many windows environments.
-					-- Remove the below condition to re-enable on windows.
+					-- Build Step for regex support in snippets. Disable on windows if necessary.
 					if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
 						return
 					end
 					return "make install_jsregexp"
 				end)(),
 				dependencies = {
-					-- `friendly-snippets` contains a variety of premade snippets.
-					--    See the README about individual language/framework/plugin snippets:
-					--    https://github.com/rafamadriz/friendly-snippets
+					-- Uncomment the block below if you want friendly-snippets
 					-- {
-					--   'rafamadriz/friendly-snippets',
+					--   "rafamadriz/friendly-snippets",
 					--   config = function()
-					--     require('luasnip.loaders.from_vscode').lazy_load()
+					--     require("luasnip.loaders.from_vscode").lazy_load()
 					--   end,
 					-- },
 				},
 			},
 			"saadparwaiz1/cmp_luasnip",
-
-			-- Adds other completion capabilities.
-			--  nvim-cmp does not ship with all sources by default. They are split
-			--  into multiple repos for maintenance purposes.
+			-- Additional completion sources
 			"hrsh7th/cmp-nvim-lsp",
 			"hrsh7th/cmp-path",
 		},
 		config = function()
-			-- See `:help cmp`
 			local cmp = require("cmp")
 			local luasnip = require("luasnip")
+
+			-- Optional: setup LuaSnip if needed
 			luasnip.config.setup({})
 
-			--NOTE: this will set the the background of popup menu as transparent
+			-- Set the background of the popup menu as transparent
 			vim.cmd("highlight Pmenu guibg=NONE")
 
-			-- For VS code icons
-			local cmp_kinds = {
-				Text = "  ",
-				Method = "  ",
-				Function = "  ",
-				Constructor = "  ",
-				Field = "  ",
-				Variable = "  ",
-				Class = "  ",
-				Interface = "  ",
-				Module = "  ",
-				Property = "  ",
-				Unit = "  ",
-				Value = "  ",
-				Enum = "  ",
-				Keyword = "  ",
-				Snippet = "  ",
-				Color = "  ",
-				File = "  ",
-				Reference = "  ",
-				Folder = "  ",
-				EnumMember = "  ",
-				Constant = "  ",
-				Struct = "  ",
-				Event = "  ",
-				Operator = "  ",
-				TypeParameter = "  ",
-			}
-			-- Define the Visual Studio Code Dark+ theme colors
+			-- Define Visual Studio Code Dark+ theme colors
 			local darkplus = {
 				black = "#1e1e1e",
 				white = "#ffffff",
@@ -844,19 +811,6 @@ require("lazy").setup({
 					end,
 				},
 				completion = { completeopt = "menu,menuone,noinsert" },
-
-				--[[ Backup Window popup colors:
-          window = {
-            completion = cmp.config.window.bordered {
-              border = 'rounded',
-              winhighlight = 'Normal:Pmenu,FloatBorder:Pmenu,Seach:None',
-            },
-            documentation = cmp.config.window.bordered {
-              border = 'rounded',
-              winhighlight = 'Normal:Pmenu,FloatBorder:Pmenu,Seach:None',
-            },
-          }, ]]
-				--
 				window = {
 					completion = cmp.config.window.bordered({
 						border = "rounded",
@@ -877,56 +831,73 @@ require("lazy").setup({
 						},
 					}),
 				},
-				-- For an understanding of why these mappings were
-				-- chosen, you will need to read `:help ins-completion`
-				--
-				-- No, but seriously. Please read `:help ins-completion`, it is really good!
 				mapping = cmp.mapping.preset.insert({
-					-- Select the [n]ext item
+					-- Select the next/previous item in the completion list.
 					["<C-n>"] = cmp.mapping.select_next_item(),
-					-- Select the [p]revious item
 					["<C-p>"] = cmp.mapping.select_prev_item(),
 
-					-- Scroll the documentation window [b]ack / [f]orward
+					-- Scroll the documentation window.
 					["<C-b>"] = cmp.mapping.scroll_docs(-4),
 					["<C-f>"] = cmp.mapping.scroll_docs(4),
 
-					-- Accept ([y]es) the completion.
-					--  This will auto-import if your LSP supports it.
-					--  This will expand snippets if the LSP sent a snippet.
+					-- Accept the completion.
 					["<Tab>"] = cmp.mapping.confirm({ select = true }),
 
-					-- Manually trigger a completion from nvim-cmp.
-					--  Generally you don't need this, because nvim-cmp will display
-					--  completions whenever it has completion options available.
+					-- Manually trigger completion.
 					["<C-Space>"] = cmp.mapping.complete({}),
 
-					-- Think of <c-l> as moving to the right of your snippet expansion.
-					--  So if you have a snippet that's like:
-					--  function $name($args)
-					--    $body
-					--  end
-					--
-					-- <c-l> will move you to the right of each of the expansion locations.
-					-- <c-h> is similar, except moving you backwards.
+					-- Jump forward in snippets
 					["<C-l>"] = cmp.mapping(function()
 						if luasnip.expand_or_locally_jumpable() then
 							luasnip.expand_or_jump()
 						end
 					end, { "i", "s" }),
+
+					-- Jump backward in snippets
 					["<C-h>"] = cmp.mapping(function()
 						if luasnip.locally_jumpable(-1) then
 							luasnip.jump(-1)
 						end
 					end, { "i", "s" }),
-
-					-- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-					--    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
 				}),
+				-- Formatting configuration to include icons
+				formatting = {
+					format = function(entry, vim_item)
+						local cmp_kinds = {
+							Text = "  ",
+							Method = "  ",
+							Function = "  ",
+							Constructor = "  ",
+							Field = "  ",
+							Variable = "  ",
+							Class = "  ",
+							Interface = "  ",
+							Module = "  ",
+							Property = "  ",
+							Unit = "  ",
+							Value = "  ",
+							Enum = "  ",
+							Keyword = "  ",
+							Snippet = "  ",
+							Color = "  ",
+							File = "  ",
+							Reference = "  ",
+							Folder = "  ",
+							EnumMember = "  ",
+							Constant = "  ",
+							Struct = "  ",
+							Event = "  ",
+							Operator = "  ",
+							TypeParameter = "  ",
+						}
+						vim_item.kind = string.format("%s %s", cmp_kinds[vim_item.kind] or "", vim_item.kind)
+						return vim_item
+					end,
+				},
 				sources = {
-					{ name = "nvim_lsp", max_item_count = 8 }, -- Limit the number of items from LSP source
-					{ name = "luasnip", max_item_count = 5 }, -- Limit the number of items from Luasnip
-					{ name = "path", max_item_count = 5 }, -- Limit the number of items from path source
+					{ name = "nvim_lsp", max_item_count = 8 }, -- Limit items from LSP source
+					{ name = "luasnip", max_item_count = 5 }, -- Limit items from Luasnip source
+					{ name = "path", max_item_count = 5 }, -- Limit items from path source
 				},
 			})
 		end,
